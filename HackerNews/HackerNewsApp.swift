fad37dc4fca1641d81f90447acd09ed2ade16328
ecemozturkk <ecemozturk704@gmail.com>
2023-04-04 21:58:37 +0300

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Ecem Öztürk on 4.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
